4edd7316ce4623bf1bbd137f3aac99070155bc6f
Anthony <theanthonyarellano@gmail.com>
2022-04-02 12:51:44 -0400

    retry adding splash

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,8 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+import Banner from './components/Banner/index';
+import SplashPage from './components/SplashPage/index';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -26,6 +28,7 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Banner />
       <NavBar />
       <Switch>
         <Route path='/login' exact={true}>
@@ -41,7 +44,7 @@ function App() {
           <User />
         </ProtectedRoute>
         <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
+          <SplashPage />
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
