c1c2272890c4129ce35840529188803e1bc56c4d
Ian_Edwards <edwards.ian.c@gmail.com>
2022-04-02 11:27:35 -0500

    model user to project relationship fixed

--- app/models/user.py
+++ app/models/user.py
@@ -11,6 +11,9 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    project = db.relationship("Project", back_populates="user", cascade="all, delete")
+
+
     @property
     def password(self):
         return self.hashed_password
