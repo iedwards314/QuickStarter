20497585d9e0835296d69078caef4496f4d41813
Ian_Edwards <edwards.ian.c@gmail.com>
2022-04-03 13:55:45 -0500

    added an alt tag that was missing

--- react-app/src/components/SplashPage/CarouselCard.js
+++ react-app/src/components/SplashPage/CarouselCard.js
@@ -7,7 +7,7 @@ const CarouselCard = () => {
             {/* Current information is only for example purposes */}
             <img
                 className="carousel-image"
-                src={"https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} />
+                src={"https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"} alt="holding a looking glass" />
             <div className='carousel-text-container'>
                 <p className='carousel-text-title'>Totally Cool Photography Pay Me Dollars</p>
                 <p className='carousel-text'>Forreal pay me idiot LOL, I promise I'm not going to steal all your money HAHAHAHA. </p>
