38773a5ee8fad382467b3a73af522569fd0b5d7c
Ian_Edwards <edwards.ian.c@gmail.com>
2022-04-06 12:15:05 -0500

    category page route is active

---
+++ react-app/src/components/CategoryPage/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { getCategories } from "../../store/project";
+import "./CategoryPage.css";
+
+const CategoryPage = () => {
+    return (
+        <h2>Category Page</h2>
+    )
+}
+
+export default CategoryPage;
