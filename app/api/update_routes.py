0c67bf0697e36356828dc5b244d9821cb4edca00
Ian_Edwards <edwards.ian.c@gmail.com>
2022-04-07 16:25:35 -0500

    added routes for updates

---
+++ app/api/update_routes.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, jsonify, request
+from app.models import Update, db
+
+reward_routes = Blueprint('updates', __name__)
+
+@update_routes.route('/')
+def updates():
+    updates = Update.query.all()
+    return {'updates': [update.to_dict() for update in updates]}
+
+@update_routes.route('/create', methods=['POST'])
+def create_update():
+    data = dict(request.json)
+    newUpdate = Update(
+        title = data['title'],
+        update=data['update'],
+        image_url=data['image_url'],
+        project_id=data['project_id'],
+    )
+    db.session.add(newUpdate)
+    db.session.commit()
+    return newUpdate.to_dict()
+
+@update_routes.route('/delete/<int:id>', methods=['DELETE'])
+def delete_update(id):
+    # data = dict(request.json)
+    update = Update.query.get(id)
+    res = {"id": id}
+    db.session.delete(update)
+    db.session.commit()
+    return res
