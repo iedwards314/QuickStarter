2ddb38de374692de031c02b4d383b66a5ba02380
Ian_Edwards <edwards.ian.c@gmail.com>
2022-04-08 10:07:29 -0500

    view updates complete

--- react-app/src/components/UpdatesFeature/index.js
+++ react-app/src/components/UpdatesFeature/index.js
@@ -2,16 +2,13 @@ import { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUpdates } from '../../store/updates';
+import './style/UpdatesFeature.css'
 
 const UpdatesFeature = ({project}) => {
     const dispatch = useDispatch()
     const sessionUser = useSelector((state) => state.session.user)
     const projectId = project?.id
     const updates = useSelector((state) => state.updates);
-    console.log("update title is...",updates[1]?.title)
-    console.log("update content is...",updates[1]?.update)
-    console.log("Object values are...", Object.values(updates))
-
 
     useEffect(() => {
         dispatch(getUpdates(projectId))
@@ -21,32 +18,30 @@ const UpdatesFeature = ({project}) => {
     let updatesArr = []
     const updatesMap = () => {
         if(updates) {
-            console.log("hitting updates array")
             updatesArr = Object.values(updates)
             console.log(updatesArr)
             return(
                 <div className='Update-list-container'>
                     <ul className='Update-list'>
-                    {updatesArr.map((update, idx)=> (
-                    <li className='Update-list-item' key={idx}>
-                        <div className='Update-count'>{idx + 1}</div>
-                        <div className='Update-title'><p>{update?.title}</p></div>
-                        <div className='Update-content'><p>{update?.update}</p></div>
-                        <div className='Update-user-image'><img src={project?.user_image} alt={project?.user} /></div>
-                    </li>
-                    ))}
+                        {updatesArr.map((update, idx)=> (
+                            <li className='Update-list-item' key={idx}>
+                                <div className='Update-count-container'><p className='Update-count'>{idx + 1}</p></div>
+                                <div className='Update-owner-image-container'><img src={project?.user_image} alt={project?.user} className='Update-owner-image'/></div>
+                                <div className='Update-detail-container'>
+                                    <div className='Update-title-container'><p className='Update-title'>{update?.title}</p></div>
+                                    <div className='Update-content-container'><p className='Update-content'>{update?.update}</p></div>
+                                </div>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             )
         }
-
-
         else return null
     }
 
     return (
         <>
-            <p>Hitting Updates</p>
             {updatesMap()}
         </>
     )
